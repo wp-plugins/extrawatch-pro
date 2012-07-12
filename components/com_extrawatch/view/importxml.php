07debe702891e6e77616bbca027a0a5274aede5e
matto3c <matto3c@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-12 06:13:24 +0000

    updated to revision 233
    git-svn-id: https://plugins.svn.wordpress.org/extrawatch-pro/trunk@571086 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- components/com_extrawatch/view/importxml.php
+++ components/com_extrawatch/view/importxml.php
@@ -3,7 +3,7 @@
  * JoomlaWatch - A real-time ajax joomla monitor and live stats
  * @package JoomlaWatch
  * @version 1.2.18
- * @revision 232
+ * @revision 233
  * @license http://www.gnu.org/licenses/gpl-3.0.txt 	GNU General Public License v3
  * @copyright (C) 2012 by Matej Koval - All rights reserved!
  * @website http://www.codegravity.com
