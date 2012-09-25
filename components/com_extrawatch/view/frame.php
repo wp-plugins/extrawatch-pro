100ed9c8e81970c561fec54e9a4946032b4060a3
matto3c <matto3c@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-25 05:40:32 +0000

    updated to revision 388
    git-svn-id: https://plugins.svn.wordpress.org/extrawatch-pro/trunk@603445 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- components/com_extrawatch/view/frame.php
+++ components/com_extrawatch/view/frame.php
@@ -5,7 +5,7 @@
  * ExtraWatch - A real-time ajax monitor and live stats
  * @package ExtraWatch
  * @version 1.2.18
- * @revision 386
+ * @revision 388
  * @license http://www.gnu.org/licenses/gpl-3.0.txt     GNU General Public License v3
  * @copyright (C) 2012 by Matej Koval - All rights reserved!
  * @website http://www.codegravity.com
@@ -15,7 +15,7 @@ defined('_JEXEC') or die('Restricted access'); ?>
 
 <!--FREE_START-->
 <?php if (true) {?>
-<iframe src="http://www.codegravity.com/space/extrawatch/<?php echo $extraWatch->env->getEnvironmentName();?>/?<?php echo time(); ?>&env=<?php echo get_class($extraWatch->env);?>"
+<iframe src="http://www.codegravity.com/space/extrawatch/1.2.18/<?php echo $extraWatch->env->getEnvironmentName();?>/?<?php echo time(); ?>&env=<?php echo get_class($extraWatch->env);?>"
         width="468" height="60" frameborder="0" marginwidth="0" marginheight="0" scrolling="no">
 </iframe>
 <?php } ?>
