7ea3e63c9fe0958c662b374e9dd7acffc6feb03f
matto3c <matto3c@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-08 16:28:36 +0000

    bugfix: fixed error in date control navigation
    git-svn-id: https://plugins.svn.wordpress.org/extrawatch-pro/trunk@665275 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- components/com_extrawatch/view/datecontrol.php
+++ components/com_extrawatch/view/datecontrol.php
@@ -5,7 +5,7 @@
  * ExtraWatch - A real-time ajax monitor and live stats
  * @package ExtraWatch
  * @version 1.2.18
- * @revision 500
+ * @revision 502
  * @license http://www.gnu.org/licenses/gpl-3.0.txt     GNU General Public License v3
  * @copyright (C) 2013 by Matej Koval - All rights reserved!
  * @website http://www.codegravity.com
@@ -17,6 +17,6 @@ defined('_JEXEC') or die('Restricted access'); ?>
   <tr>
     <td align='left'><?php echo("<a href='javascript:extrawatch_setDay($prev)' id='$prev'>&lt;" . ExtraWatchDate::getDateByDay($prev) . "<img src='" . $this->extraWatch->config->getLiveSiteWithSuffix() . "components/com_extrawatch/img/icons/calendar.gif' border='0' align='center' /></a>"); ?></td>
     <td align='center'><?php if ($day != $today) echo("<a href='javascript:extrawatch_setDay($today)' id='$today'>" . _EW_STATS_TODAY . "</a>"); ?></td>
-    <td align='right'><?php if ($next <= $today) echo("<a href='javascript:extrawatch_setDay($next)' id='$next'><img src='" . $extraWatch->config->getLiveSiteWithSuffix() . "components/com_extrawatch/img/icons/calendar.gif' border='0' align='center' />" . ExtraWatchDate::getDateByDay($next) . "&gt;</a>"); ?></td>
+    <td align='right'><?php if ($next <= $today) echo("<a href='javascript:extrawatch_setDay($next)' id='$next'><img src='" . $this->extraWatch->config->getLiveSiteWithSuffix() . "components/com_extrawatch/img/icons/calendar.gif' border='0' align='center' />" . ExtraWatchDate::getDateByDay($next) . "&gt;</a>"); ?></td>
   </tr>
 </table>
--- src/env/wordpress/readme.txt
+++ src/env/wordpress/readme.txt
@@ -4,13 +4,15 @@ Donate link: http://www.extrawatch.com/
 Tags: admin, widget, plugin, counter, anti-spam, stats, visitors, click map, countries, tracking, maps, location, geolocation, statistics, spam, flags, goals
 Requires at least: 3.2.1.
 Tested up to: 3.2.1.
-Stable tag: 1.2.18.500
+Stable tag: 1.2.18.502
 
 Popular visitor live tracking component ported from Joomla. Features: live stats,graphs,goals,history,anti-spam,blocking,nightly reports and more..
 
 == Description ==
 Popular visitor live tracking component ported from Joomla. 
 ExtraWatch allows you to watch your joomla visitors live stats and bots in real-time from the administration back-end. 
+
+For more information and demos please visit: http://www.extrawatch.com
 
 Specially their IP addresses, countries they come from, geographical location on a map, which pages they are viewing, their browser and operating system, it creates daily and all-time stats from these information plus unique, pageload and total hits statistics. 
 Furthermore, you can block harmful IP addresses, see blocked attempts stats, evaluate the trend charts, and create goals based on many parameters. In the front-end, it displays the top countries, user and visit information for certain periods of time.
