3b4b5bcaa8b37889f27be8182e684305918ca326
matto3c <matto3c@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-06 07:43:38 +0000

    documentation: added also wordpress manual installation method into INSTALL.txt
    git-svn-id: https://plugins.svn.wordpress.org/extrawatch-pro/trunk@676797 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- components/com_extrawatch/view/importxml.php
+++ components/com_extrawatch/view/importxml.php
@@ -3,7 +3,7 @@
  * JoomlaWatch - A real-time ajax joomla monitor and live stats
  * @package JoomlaWatch
  * @version 1.2.18
- * @revision 541
+ * @revision 543
  * @license http://www.gnu.org/licenses/gpl-3.0.txt 	GNU General Public License v3
  * @copyright (C) 2013 by Matej Koval - All rights reserved!
  * @website http://www.codegravity.com
