a0fb4909093f3e8d994865880badebdcbfa503f9
matto3c <matto3c@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-11 06:50:20 +0000

    build: fixed replacement of FREE_START / FREE_END tags
    git-svn-id: https://plugins.svn.wordpress.org/extrawatch-pro/trunk@679549 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- components/com_extrawatch/view/frame.php
+++ components/com_extrawatch/view/frame.php
@@ -5,7 +5,7 @@
  * ExtraWatch - A real-time ajax monitor and live stats
  * @package ExtraWatch
  * @version 1.2.18
- * @revision 553
+ * @revision 555
  * @license http://www.gnu.org/licenses/gpl-3.0.txt     GNU General Public License v3
  * @copyright (C) 2013 by Matej Koval - All rights reserved!
  * @website http://www.codegravity.com
@@ -13,10 +13,6 @@
 
 defined('_JEXEC') or die('Restricted access'); ?>
 
-<!--FREE_START-->
-<iframe src="http://www.codegravity.com/space/extrawatch/?time=<?php echo time(); ?>&env=<?php echo get_class($extraWatch->env);?>"
-        width="468" height="60" frameborder="0" marginwidth="0" marginheight="0" scrolling="no">
-</iframe>
-<!--FREE_END-->
+
 
 
--- components/com_extrawatch/view/stats-today.php
+++ components/com_extrawatch/view/stats-today.php
@@ -5,7 +5,7 @@
  * ExtraWatch - A real-time ajax monitor and live stats
  * @package ExtraWatch
  * @version 1.2.18
- * @revision 553
+ * @revision 555
  * @license http://www.gnu.org/licenses/gpl-3.0.txt     GNU General Public License v3
  * @copyright (C) 2013 by Matej Koval - All rights reserved!
  * @website http://www.codegravity.com
@@ -15,7 +15,7 @@ defined('_JEXEC') or die('Restricted access'); ?>
 <table border="0">
   <h3><?php echo(_EW_STATS_DAILY_TITLE) . "&nbsp;"; echo $extraWatch->date->getDateByDay($day);?><?php echo $extraWatchHTML->renderOnlineHelp("daily-stats"); ?></h3>
   <tr>
-    <td>
+    <td colspan="6">
       <?php echo $extraWatchHTML->renderDateControl(); ?>
     </td>
   </tr>
