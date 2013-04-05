77822ff433dc9bbf5ce4ffbc66470a29a0d758e4
matto3c <matto3c@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-05 15:47:51 +0000

    bugfix: showing #encrypted# even if there were keywords in came from url
    git-svn-id: https://plugins.svn.wordpress.org/extrawatch-pro/trunk@692310 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- components/com_extrawatch/src/class.extrawatch.visit.php
+++ components/com_extrawatch/src/class.extrawatch.visit.php
@@ -5,7 +5,7 @@
  * ExtraWatch - A real-time ajax monitor and live stats
  * @package ExtraWatch
  * @version 2.0
- * @revision 607
+ * @revision 608
  * @license http://www.gnu.org/licenses/gpl-3.0.txt     GNU General Public License v3
  * @copyright (C) 2013 by CodeGravity.com - All rights reserved!
  * @website http://www.extrawatch.com
@@ -317,7 +317,7 @@ class ExtraWatchVisit
    */
   function extractPhraseFromUrl($referer)
   {
-    if (@strstr($referer, "usg=")) {
+    if (@strstr($referer, "usg=") && @strstr($referer, "q=&")) {
         return "#encrypted#";
     }
     if (@strstr($referer, "q=")) { // keywords from google
