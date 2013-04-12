806205cd4da13c771ac70cb003ab71d638274ba7
matto3c <matto3c@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-12 10:09:31 +0000

    bugfix: clicking of changing live site had to be done twice
    git-svn-id: https://plugins.svn.wordpress.org/extrawatch-pro/trunk@696510 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- components/com_extrawatch/src/class.extrawatch.config.php
+++ components/com_extrawatch/src/class.extrawatch.config.php
@@ -5,7 +5,7 @@
  * ExtraWatch - A real-time ajax monitor and live stats
  * @package ExtraWatch
  * @version 2.0
- * @revision 619
+ * @revision 620
  * @license http://www.gnu.org/licenses/gpl-3.0.txt     GNU General Public License v3
  * @copyright (C) 2013 by CodeGravity.com - All rights reserved!
  * @website http://www.extrawatch.com
@@ -160,7 +160,7 @@ class ExtraWatchConfig
       $query = sprintf("insert into #__extrawatch_config values ('','%s','%s')", $this->database->getEscaped($key), $this->database->getEscaped($value));
       $this->database->executeQuery($query);
     }
-
+    $this->reloadConfigValues();
   }
 
   /**
