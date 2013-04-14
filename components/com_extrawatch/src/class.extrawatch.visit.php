c169de9e23dd563380d459529a6aa25f7e0947a9
matto3c <matto3c@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-14 20:05:26 +0000

    bugfix: referer fix
    git-svn-id: https://plugins.svn.wordpress.org/extrawatch-pro/trunk@697428 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- components/com_extrawatch/src/class.extrawatch.visit.php
+++ components/com_extrawatch/src/class.extrawatch.visit.php
@@ -5,7 +5,7 @@
  * ExtraWatch - A real-time ajax monitor and live stats
  * @package ExtraWatch
  * @version 2.0
- * @revision 624
+ * @revision 626
  * @license http://www.gnu.org/licenses/gpl-3.0.txt     GNU General Public License v3
  * @copyright (C) 2013 by CodeGravity.com - All rights reserved!
  * @website http://www.extrawatch.com
@@ -856,7 +856,7 @@ class ExtraWatchVisit
     $refererWithoutPrefix = str_replace($ignorePrefix, "", $referer);
     $liveSiteWithoutPrefix = str_replace($ignorePrefix, "", $liveSite);
     $comparison = @stripos($refererWithoutPrefix, $liveSiteWithoutPrefix);
-    if ($comparison => 0) { //is on 0th position and matches
+    if ($comparison >= 0) { //is on 0th position and matches
       return TRUE;
     } else {
       return FALSE;
