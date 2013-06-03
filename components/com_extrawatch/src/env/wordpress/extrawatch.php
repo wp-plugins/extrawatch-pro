4fb0e38774b82e89214472520daa92053c17b1e8
matto3c <matto3c@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-03 17:00:50 +0000

    wordpress: widget not published in meta section by default
    git-svn-id: https://plugins.svn.wordpress.org/extrawatch-pro/trunk@722169 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- components/com_extrawatch/src/env/wordpress/extrawatch.php
+++ components/com_extrawatch/src/env/wordpress/extrawatch.php
@@ -3,7 +3,7 @@
 Plugin Name: ExtraWatch Live Stats and Visitor Counter PRO
 Plugin URI: http://www.extrawatch.com
 Description: Popular visitor live tracking component ported from Joomla. <a href="http://www.extrawatch.com/demo/extrawatch/1.2.18/wordpress/">View demo video</a>. Demo server: <a href="http://codegravitydemo.com/wordpress/PRO/">Frontpage</a> / <a href="http://codegravitydemo.com/wordpress/PRO/wp-admin/">Admin</a>. Features: <strong>Visitor Live Stats</strong>, <strong>History</strong>, <strong>Graphs</strong>, <strong>Anti-spam</strong>, <strong>Nightly Email Reports</strong>, <strong>Front-end Counters</strong>, translated in <strong>42 world languages</strong>
-Version: 2.0.737 PRO
+Version: 2.0.738 PRO
 Author: CodeGravity.com
 Author URI: http://www.extrawatch.com
 */
@@ -29,7 +29,7 @@ if (@function_exists("extrawatch_admin_menu")) {
 } else {
 
   add_action('admin_menu', 'extrawatch_admin_menu');
-  add_action('wp_meta', 'extrawatch_frontend');
+  //add_action('wp_meta', 'extrawatch_frontend');
 
   require_once(JPATH_BASE2 . DS . "components" . DS . "com_extrawatch" . DS . "src" . DS. "inc.extrawatch.env.php");
 
