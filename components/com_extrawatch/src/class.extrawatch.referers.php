1a81b088d1f29d836753069a859f43580d97329f
matto3c <matto3c@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-23 16:15:40 +0000

    downloads: fix of class not found class.extrawatch.referers.php for wordpress
    git-svn-id: https://plugins.svn.wordpress.org/extrawatch-pro/trunk@730424 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ components/com_extrawatch/src/class.extrawatch.referers.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @file
+ * ExtraWatch - A real-time ajax monitor and live stats
+ * @package ExtraWatch
+ * @version 2.1
+ * @revision 767
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt     GNU General Public License v3
+ * @copyright (C) 2013 by CodeGravity.com - All rights reserved!
+ * @website http://www.extrawatch.com
+ */
+
+/** ensure this file is being included by a parent file */
+if (!defined('_JEXEC') && !defined('_VALID_MOS'))
+  die('Restricted access');
+
+class ExtraWatchReferer
+{
+  public $database;
+  public $config;
+  public $helper;
+  public $env;
+  public $date;
+  public $stat;
+
+ function __construct($database)
+  {
+    $this->database = $database;
+    $this->env = ExtraWatchEnvFactory::getEnvironment();
+    $this->config = new ExtraWatchConfig($this->database);
+    $this->helper = new ExtraWatchHelper($this->database);
+    $this->date = new ExtraWatchDate($this->database);
+	$this->stat = new ExtraWatchStat($this->database);
+  }
+  
+function getReferer($url){
+	$host="";
+    $socialmedia=unserialize(EXTRAWATCH_SOCIAL_MEDIA_REGEX);
+    foreach($socialmedia as  $key => $value)
+    {
+        if (preg_match($key,$url)==1) {
+            $host=$value;
+        }
+    }
+    return $host;
+}
+function checkCameFrom($url){;
+    $referer= $this->getReferer($url);
+        if (@$referer) {
+	        $this->stat->increaseKeyValueInGroup($this->config->getConfigValue("EW_DB_KEY_SOCIAL_MEDIA"),$referer);
+        }
+	}
+}
