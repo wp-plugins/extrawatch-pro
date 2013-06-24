a4aff0d35fe07bb7912e08d8625aff52853e5fe1
matto3c <matto3c@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-24 17:49:14 +0000

    goals: transparent icons in goals section
    git-svn-id: https://plugins.svn.wordpress.org/extrawatch-pro/trunk@731005 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- components/com_extrawatch/view/importxml.php
+++ components/com_extrawatch/view/importxml.php
@@ -3,7 +3,7 @@
  * JoomlaWatch - A real-time ajax joomla monitor and live stats
  * @package JoomlaWatch
  * @version 2.1
- * @revision 779
+ * @revision 780
  * @license http://www.gnu.org/licenses/gpl-3.0.txt 	GNU General Public License v3
  * @copyright (C) 2013 by CodeGravity.com - All rights reserved!
  * @website http://www.codegravity.com
