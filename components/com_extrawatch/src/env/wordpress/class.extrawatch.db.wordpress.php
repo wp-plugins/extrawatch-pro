8258113e65e762bcfb4fab55df3a6dcd91f629ec
matto3c <matto3c@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-12 12:17:43 +0000

    wordpress: cannot connect bug
    git-svn-id: https://plugins.svn.wordpress.org/extrawatch-pro/trunk@819743 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- components/com_extrawatch/src/env/wordpress/class.extrawatch.db.wordpress.php
+++ components/com_extrawatch/src/env/wordpress/class.extrawatch.db.wordpress.php
@@ -5,7 +5,7 @@
  * ExtraWatch - A real-time ajax monitor and live stats
  * @package ExtraWatch
  * @version 2.2
- * @revision 1421
+ * @revision 1420
  * @license http://www.gnu.org/licenses/gpl-3.0.txt     GNU General Public License v3
  * @copyright (C) 2013 by CodeGravity.com - All rights reserved!
  * @website http://www.extrawatch.com
@@ -17,7 +17,7 @@ class ExtraWatchDBWrapWordpress implements ExtraWatchDBWrap
 {
 
   public $query;
-  public static $dbref;
+  public $dbref;
   public $result;
   public $dbprefix;
   public $errNum;
@@ -32,68 +32,62 @@ class ExtraWatchDBWrapWordpress implements ExtraWatchDBWrap
     $database = $wpdb->dbname;
     $this->dbprefix = $wpdb->base_prefix;
     $select = TRUE;
-	$driver = "mysql";
 
-	if (!$this->dbref) {
-		$this->dbref = new PDO("$driver:host=$host;dbname=$database", $user, $password);
-	}
-
+    if (!($this->dbref = @mysql_connect($host, $user, $password, TRUE))) {
+      die("cannot connect");
+    }
+    if ($select) {
+      $this->select($database);
+    }
   }
 
   function __destruct()
   {
-    //return mysql_close($this->dbref);
+    return @mysql_close($this->dbref);
   }
 
   function getEscaped($sql)
   {
-    return $sql;
+    return mysql_real_escape_string($sql, $this->dbref);
   }
 
   function query()
   {
     $sql = $this->query;
-    $sql = $this->replaceDbPrefix($sql);
-    $STH = $this->dbref->query($sql);
-    if (!$STH) {
-      return null;
+	ExtraWatchLog::debug("query: $sql"); 
+	$sql = str_replace("#__", $this->dbprefix, $sql);
+    $this->result = mysql_query($sql, $this->dbref);
+
+    if (!$this->result) {
+      $this->errNum = mysql_errno($this->dbref);
+      $this->errMsg = mysql_error($this->dbref) . " in query $sql";
+      return FALSE;
     }
-    $STH->setFetchMode(PDO::FETCH_ASSOC);
-    $result = $STH->fetch();
-    return $result;
+    return $this->result;
   }
 
   function loadResult()
   {
-    $this->query = $this->replaceDbPrefix($this->query);
-    $STH = $this->dbref->query($this->query);
-    if (!$STH) {
+    if (!($result = $this->query())) {
       return null;
     }
-    $STH->setFetchMode(PDO::FETCH_ASSOC);
-
     $return = null;
-    if ($row = $STH->fetch()) {
-      $return = @$row['value'];
-      if (!$return) {
-        $key = @key($row);
-        $return = @$row[$key];
-      }
+    if ($row = mysql_fetch_row($result)) {
+      $return = $row[0];
     }
+    mysql_free_result($result);
     return $return;
   }
 
   function loadAssocList($key = '')
   {
-    $this->query = $this->replaceDbPrefix($this->query);
-    $STH = $this->dbref->query($this->query);
-    $STH->setFetchMode(PDO::FETCH_ASSOC);
-
-    $return = null;
-    if ($row = $STH->fetchAll()) {
-      $return = @$row['value'];
+    $result = $this->query();
+    $array = array();
+    while ($row = mysql_fetch_assoc($result)) {
+      $array[] = $row;
     }
-    return $return;
+    mysql_free_result($result);
+    return $array;
   }
 
   function select($database)
@@ -110,7 +104,8 @@ class ExtraWatchDBWrapWordpress implements ExtraWatchDBWrap
 
   function setQuery($query)
   {
-    $this->query = $query;
+	ExtraWatchLog::debug("setQuery: $query"); 
+	$this->query = $query;
   }
 
   function getErrorNum()
@@ -120,7 +115,6 @@ class ExtraWatchDBWrapWordpress implements ExtraWatchDBWrap
 
   function objectListQuery($query)
   {
-	ExtraWatchLog::debug("objectListQuery: $query");
     $this->query = $query;
     return $this->loadObjectList();
   }
@@ -132,42 +126,45 @@ class ExtraWatchDBWrapWordpress implements ExtraWatchDBWrap
 
   function resultQuery($query)
   {
-	ExtraWatchLog::debug("resultQuery: $query");
     $this->query = $query;
+    $this->setQuery($query);
     return $this->loadResult();
   }
 
   function executeQuery($query)
   {
-	ExtraWatchLog::debug("executeQuery: $query");
     $this->query = $query;
     return $this->query();
   }
 
   function assocListQuery($query)
   {
-	ExtraWatchLog::debug("assocListQuery: $query");
     $this->query = $query;
     return $this->loadAssocList();
   }
 
   function replaceDbPrefix($sql)
   {
-    return str_replace("#__", $this->dbprefix, $sql);
+   	ExtraWatchLog::debug("$sql");  
+	return str_replace("#__", $this->dbprefix, $sql);
   }
 
   private function loadObjectList($key = '')
   {
-    $sql = $this->replaceDbPrefix($this->query);
-    $STH = $this->dbref->query($sql);
-    if (!$STH) {
+    if (!($cur = $this->query())) {
       return null;
     }
-    $STH->setFetchMode(PDO::FETCH_OBJ);
-    $result = $STH->fetchAll();
-    return $result;
+    $array = array();
+    while ($row = mysql_fetch_object($cur)) {
+      if ($key) {
+        $array[$row->$key] = $row;
+      } else {
+        $array[] = $row;
+      }
+    }
+    mysql_free_result($cur);
+    return $array;
   }
-
 }
 
 
