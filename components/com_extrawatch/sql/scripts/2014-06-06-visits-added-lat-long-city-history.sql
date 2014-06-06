909127694e90d1fb4202bd69aea99a204be02559
matto3c <matto3c@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-06 16:44:38 +0000

    update to revision 1970
    git-svn-id: https://plugins.svn.wordpress.org/extrawatch-pro/trunk@927861 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ components/com_extrawatch/sql/scripts/2014-06-06-visits-added-lat-long-city-history.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `#__extrawatch_history` ADD `city` VARCHAR( 255 ) NULL DEFAULT NULL;
+ALTER TABLE `#__extrawatch_history` ADD `latitude` FLOAT NULL DEFAULT NULL; 
+ALTER TABLE `#__extrawatch_history` ADD `longitude` FLOAT NULL DEFAULT NULL;
+ 
