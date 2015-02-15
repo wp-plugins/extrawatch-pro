465ff7ddd6f2fc4a89246288f0e02ce43fd37bf5
matto3c <matto3c@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-15 18:34:26 +0000

    fix: ExtraWatchJoomlaEnv::getDatabase() must be compatible with ExtraWatchEnv::getDatabase($user = '') in class.extrawatch.env.joomla.php on line 20
    git-svn-id: https://plugins.svn.wordpress.org/extrawatch-pro/trunk@1090629 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- components/com_extrawatch/src/env/magento/app/code/local/CodeGravity/Extrawatch/Helper/Data.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-class CodeGravity_ExtraWatch_Helper_Data extends Mage_Core_Helper_Abstract {
-
-}
--- components/com_extrawatch/src/env/magento/app/code/local/CodeGravity/Extrawatch/controllers/Adminhtml/Controller.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class CodeGravity_ExtraWatch_Adminhtml_Controller extends Mage_Adminhtml_Controller_action
-{
-
-
-}
